532ba40838c3ef521f9fe19b168bcfffd5cbd579
Unlife86 <der_schatten_ui@mail.ru>
2017-12-26 22:33:12 +0700

    Filter search for the standings and matches

--- frontend/views/pages/_season.php
+++ frontend/views/pages/_season.php
@@ -5,22 +5,21 @@ use yii\widgets\Menu;
 use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
 $name_t = [
-    '2' => ['1 тур', '2 тур', 'Полуфинал', 'Финал'],
+    '2' => [/*'1/8 финала', */'1/4 финала', 'Полуфинал', 'Полуфинал: Ответные матчи', 'Финал'],
     '4' => ['Полуфинал', '3 место', 'Финал'],
 ];
 
 ?>
 <?php $form = ActiveForm::begin(['action' => [$action],'method' => 'get',]); ?>
-    <?= Html::dropDownList('season', $current['season'],ArrayHelper::map($dropList['seasons'], 'id_season', 'idSeason.year'),['class' => 'form-control', 'onchange'=>'this.form.submit()']);?>
+    <?= Html::dropDownList('season', $current['id_season'],ArrayHelper::map($dropList['seasons'], 'id_season', 'idSeason.year'),['class' => 'form-control', 'onchange'=>'this.form.submit()']);?>
 <?php ActiveForm::end(); ?>
 <?php
 $itemsUl = [];
-
 foreach ($dropList['tours'] as $tour):
     if(array_key_exists($tour->id_league, $name_t)) {
-        $itemLi = ['label' => $name_t[$tour->id_league][$tour->n_tour - 1], 'url' => ['/pages/'.$action, 'idLeague' => $tour->id_league, 'tour' => $tour->n_tour, 'season' => $current['season']]];
+        $itemLi = ['label' => $name_t[$tour->id_league][$tour->n_tour - 1], 'url' => ['/pages/'.$action, 'idLeague' => $tour->id_league, 'tour' => $tour->n_tour, 'season' => $current['id_season']]];
     } else {
-        $itemLi = ['label' => $tour->n_tour.' тур', 'url' => ['/pages/'.$action, 'idLeague' => $tour->id_league, 'tour' => $tour->n_tour, 'season' => $current['season']]];
+        $itemLi = ['label' => $tour->n_tour.' тур', 'url' => ['/pages/'.$action, 'idLeague' => $tour->id_league, 'tour' => $tour->n_tour, 'season' => $current['id_season']]];
     }
     array_push($itemsUl, $itemLi);
 endforeach;
--- frontend/views/pages/_submenu.php
+++ frontend/views/pages/_submenu.php
@@ -6,20 +6,20 @@ use yii\widgets\Menu;
         <?php
             if ($current['id_league'] == 4) {
                 $itemsUl = [
-                    ['label' => 'Календарь игр', 'url' => ['/pages/results', 'idLeague' => $current['id_league'], 'tour' => $current['tour'], 'season' => $current['season']]],
-                    ['label' => $names['group'], 'url' => ['/pages/tournament', 'idLeague' => 3, 'tour' => Yii::$app->currentFootballData->getCurrentTour($current['season'], 3), 'season' => $current['season']]],
-                    ['label' => $names['play-off'], 'url' => ['/pages/tournament', 'idLeague' => 4, 'tour' => $current['tour'], 'season' => $current['season']]]
+                    ['label' => 'Календарь игр', 'url' => ['/pages/results', 'idLeague' => $current['id_league'], 'tour' => $current['n_tour'], 'season' => $current['id_season']]],
+                    ['label' => $names['group'], 'url' => ['/pages/tournament', 'idLeague' => 3, 'tour' => Yii::$app->currentFootballData->getCurrentTour($current['id_season'], 3), 'season' => $current['id_season']]],
+                    ['label' => $names['play-off'], 'url' => ['/pages/tournament', 'idLeague' => 4, 'tour' => $current['n_tour'], 'season' => $current['id_season']]]
                 ];
             } else if (($current['id_league'] == 3) and (Yii::$app->currentFootballData->getCurrentLeague($current['id_league'])->current == 0)) {
                 $itemsUl = [
-                    ['label' => 'Календарь игр', 'url' => ['/pages/results', 'idLeague' => 4, 'tour' => Yii::$app->currentFootballData->getCurrentTour($current['season'], 4), 'season' => $current['season']]],
-                    ['label' => $names['group'], 'url' => ['/pages/tournament', 'idLeague' => 3, 'tour' => Yii::$app->currentFootballData->getCurrentTour($current['season'], 3), 'season' => $current['season']]],
-                    ['label' => $names['play-off'], 'url' => ['/pages/tournament', 'idLeague' => 4, 'tour' => Yii::$app->currentFootballData->getCurrentTour($current['season'], 4), 'season' => $current['season']]]
+                    ['label' => 'Календарь игр', 'url' => ['/pages/results', 'idLeague' => 4, 'tour' => Yii::$app->currentFootballData->getCurrentTour($current['id_season'], 4), 'season' => $current['id_season']]],
+                    ['label' => $names['group'], 'url' => ['/pages/tournament', 'idLeague' => 3, 'tour' => Yii::$app->currentFootballData->getCurrentTour($current['id_season'], 3), 'season' => $current['id_season']]],
+                    ['label' => $names['play-off'], 'url' => ['/pages/tournament', 'idLeague' => 4, 'tour' => Yii::$app->currentFootballData->getCurrentTour($current['id_season'], 4), 'season' => $current['id_season']]]
                 ];
             } else {
                 $itemsUl = [
-                    ['label' => 'Календарь игр', 'url' => ['/pages/results', 'idLeague' => $current['id_league'], 'tour' => $current['tour'], 'season' => $current['season']]],
-                    ['label' => $names[$current['type_league']], 'url' => ['/pages/tournament', 'idLeague' => $current['id_league'], 'tour' => $current['tour'], 'season' => $current['season']]],
+                    ['label' => 'Календарь игр', 'url' => ['/pages/results', 'idLeague' => $current['id_league'], 'tour' => $current['n_tour'], 'season' => $current['id_season']]],
+                    ['label' => $names[$current['type_league']], 'url' => ['/pages/tournament', 'idLeague' => $current['id_league'], 'tour' => $current['n_tour'], 'season' => $current['id_season']]],
                 ];
             }
             echo Menu::widget([
