c62081e705c8d89333783d2fcaed7d4cac744a53
Unlife86 <der_schatten_ui@mail.ru>
2017-12-26 22:35:41 +0700

    Assets of the frontend app

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -33,5 +33,7 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'sersid\fontawesome\Asset',
+        'frontend\assets\HeadAsset'
     ];
 }
