821abd51b2668c67dada0c30bde3b4606e885487
Unlife86 <der_schatten_ui@mail.ru>
2017-12-26 21:22:03 +0700

    Setting up the project

--- common/config/main.php
+++ common/config/main.php
@@ -1,11 +1,35 @@
 <?php
 return [
     'language' => 'ru-RU',
-    'timeZone' => 'Asia/Novokuznetsk',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'currentFootballData' => [
+            'class' => 'common\components\CurrentFootballData',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+    ],
+    'modules' => [
+        'media' => [
+            'class' => 'common\modules\media\MediaModule',
+            'layoutPath' => '@app/views/layout',
         ],
     ],
 ];
--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'adminEmail' => 'der_schatten_ui@mail.ru',
-    'bossEmail' => 's.shamin@lenkuz.ru',
+    'bossEmail' => 's.shamin@tclk.ru',
     'user.passwordResetTokenExpire' => 3600,
+    'allowedIPs' => ['127.0.0.1', '10.2.2.10', '10.2.2.3', '176.96.65.110'],
 ];
