121af6edf7e7106943f532e15d949931741aa279
Unlife86 <der_schatten_ui@mail.ru>
2018-02-14 00:33:43 +0700

    Assets of the backend app

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -21,10 +21,9 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
-        'js/main.js'
+        'js/main.js',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'dmstr\web\AdminLteAsset'
     ];
 }
